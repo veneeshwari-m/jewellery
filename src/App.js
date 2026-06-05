6d79106e2f1f673bcaad0f9334853c53c58d97d1
Iniya <veneeshwarim@email.com>
2026-06-05 18:10:27 +0530

    mobile-view fixed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
 import Home from './components/Home/Home';
+import CreateAccount from './pages/CreateAccount/CreateAccount';
+import ContactUs from './pages/ContactUs/ContactUs';
+import EnquiryForm from './pages/EnquiryForm/EnquiryForm';
+import Login from './pages/Login/Login';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <Layout>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create-account" element={<CreateAccount />} />
+            <Route path="/contact-us" element={<ContactUs />} />
+            <Route path="/enquiry-form" element={<EnquiryForm />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+      </Layout>
+    </Router>
   );
 }
 
